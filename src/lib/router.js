3e6d9e6b3d7f213c777355d47064391d8cf21c9c
Tim Rodger <tim.rodger@gmail.com>
2015-04-07 23:04:54 +0100

    fixes bug in requiring a handler module

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,5 +1,5 @@
 var NewWordHandler = require('./new_word_handler'),
-    UpdatedCellHandler = require('./new_word_handler');
+    UpdatedCellHandler = require('./updated_cell_handler');
 
 /**
  * @param pub socket to write facts back to
@@ -7,7 +7,7 @@ var NewWordHandler = require('./new_word_handler'),
  */
 module.exports.newFact = function(pub, fact) {
     if (fact.name == 'word.new') {
-        NewWordHandler.handleFact(pub,fact);
+        NewWordHandler.handleFact(pub, fact);
     }  else if (fact.name == 'cell.updated') {
         UpdatedCellHandler.handleFact(pub, fact);
     }
