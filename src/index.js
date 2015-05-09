5f1b556503071413babb0f32675a433e54bf23c3
Tim Rodger <tim.rodger@gmail.com>
2015-05-09 16:11:10 +0100

    Uses same index.js file as monitor app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var logger = require('./lib/logger'),
     rabbitmq = require('rabbit.js'),
     router = require('./lib/router');
 
-logger.info('most-solved: running');
+logger.info('running');
 
 var context = rabbitmq.createContext(
     'amqp://' + process.env.RABBITMQ_PORT_5672_TCP_ADDR + ':' + process.env.RABBITMQ_PORT_5672_TCP_PORT
@@ -10,14 +10,14 @@ var context = rabbitmq.createContext(
 
 context.on('ready', function() {
 
-    logger.info('most-solved: connected');
+    logger.info('connected');
 
-    // subscribe to pub and sub queues
     var sub = context.socket('SUB'),
         pub = context.socket('PUB');
 
     pub.connect('events', function() {
         sub.connect('events', function () {
+
             // deal with facts as they come in
             sub.on('data', function (body) {
                 router.newFact(pub, JSON.parse(body));
